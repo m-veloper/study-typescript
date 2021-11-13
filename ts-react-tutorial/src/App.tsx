90534d86993a429723e13b698feee0d30595b323
m-veloper <m_veloper@naver.com>
2021-11-14 03:41:42 +0900

    타입스크립트 리액트 프로젝트 생성

---
+++ ts-react-tutorial/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Greetings from "./Component/Greetings";
+
+const App: React.FC = () => {
+  const onClick = (name:string) => {
+    console.log(name);
+  }
+  return (
+    <Greetings name={"리액트"} onClick={onClick}/>
+);
+}
+
+export default App;
