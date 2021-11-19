5aa6dd5649c9729c5962408e7a0b02acc7e01b1f
m-veloper <m_veloper@naver.com>
2021-11-20 02:17:33 +0900

    typesafe-actions 적용 및 리팩토링

--- ts-react-redux-tutorial/src/module/counter.ts
+++ ts-react-redux-tutorial/src/module/counter.ts
@@ -1,40 +1,75 @@
-const INCREASE = "counter/INCREASE" as const;
-const DECREASE = "counter/DECREASE" as const;
-const INCREASE_BY = "counter/INCREASE_BY" as const;
+/**
+ * typesafe-action을 사용할 경우
+ * npm install typesafe-actions
+ */
+import {deprecated, ActionType, createReducer, action} from 'typesafe-actions';
 
-export const increase = () => ({type: INCREASE});
-export const decrease = () => ({type: DECREASE});
-export const increaseBy = (diff: number) => ({
-  type: INCREASE_BY,
-  payload: diff
-});
+const {createStandardAction} = deprecated;
+
+
+export const increase = createStandardAction("counter/INCREASE")();
+export const decrease = createStandardAction("counter/DECREASE")();
+export const increaseBy = createStandardAction("counter/INCREASE_BY")<number>();
 
 type CounterState = {
-  count: number
+    count: number
 };
 
 const initialState: CounterState = {
-  count: 0
+    count: 0
 }
 
-type CounterAction =
-  | ReturnType<typeof increase>
-  | ReturnType<typeof decrease>
-  | ReturnType<typeof increaseBy>
+const actions = {increase, decrease, increaseBy};
+type CounterAction = ActionType<typeof actions>;
 
-function counter(state: CounterState = initialState, action: any): CounterState {
-  switch (action.type) {
-    case INCREASE:
-      return {count: state.count + 1};
-    case DECREASE:
-      return {count: state.count - 1};
-    case INCREASE_BY:
-      return {count: state.count + action.payload};
-    default:
-      return state;
-  }
-}
+const counter = createReducer<CounterState, CounterAction>(initialState)
+    .handleAction(increase, state => ({count: state.count + 1}))
+    .handleAction(decrease, state => ({count: state.count - 1}))
+    .handleAction(increaseBy, (state, action) => ({count: state.count + action.payload}))
 
 export default counter;
+
+
+/**
+ * typesafe-action을 사용하지 않을 경우
+ */
+// const INCREASE = "counter/INCREASE" as const;
+// const DECREASE = "counter/DECREASE" as const;
+// const INCREASE_BY = "counter/INCREASE_BY" as const;
+//
+// export const increase = () => ({type: INCREASE});
+// export const decrease = () => ({type: DECREASE});
+// export const increaseBy = (diff: number) => ({
+//   type: INCREASE_BY,
+//   payload: diff
+// });
+//
+// type CounterState = {
+//   count: number
+// };
+//
+// const initialState: CounterState = {
+//   count: 0
+// }
+//
+// type CounterAction =
+//   | ReturnType<typeof increase>
+//   | ReturnType<typeof decrease>
+//   | ReturnType<typeof increaseBy>
+//
+// function counter(state: CounterState = initialState, action: any): CounterState {
+//   switch (action.type) {
+//     case INCREASE:
+//       return {count: state.count + 1};
+//     case DECREASE:
+//       return {count: state.count - 1};
+//     case INCREASE_BY:
+//       return {count: state.count + action.payload};
+//     default:
+//       return state;
+//   }
+// }
+//
+// export default counter;
 
 
--- ts-react-redux-tutorial/src/module/todo.ts
+++ ts-react-redux-tutorial/src/module/todo.ts
@@ -1,6 +1,10 @@
-const ADD_TODO = 'todos/ADD_TODO' as const;
-const TOGGLE_TODO = 'todos/TOGGLE_TODO' as const;
-const REMOVE_TODO = 'todos/REMOVE_TODO' as const;
+import {deprecated, ActionType, createReducer, action} from 'typesafe-actions';
+
+const {createStandardAction} = deprecated;
+
+const ADD_TODO = 'todos/ADD_TODO' as const; // 파라미터로 받아오는 값이랑, 페이로드값이랑 다른 경우는 createAction을 사용하지 않는 편이 깔끔할 수 있다.
+const TOGGLE_TODO = 'todos/TOGGLE_TODO';
+const REMOVE_TODO = 'todos/REMOVE_TODO';
 
 let nextId = 1;
 
@@ -12,20 +16,15 @@ export const addTodo = (text: string) => ({
     }
 })
 
-export const toggleTodo = (id: number) => ({
-    type: TOGGLE_TODO,
-    payload: id
-});
+export const toggleTodo = createStandardAction(TOGGLE_TODO)<number>();
+export const removeTodo = createStandardAction(REMOVE_TODO)<number>();
 
-export const removeTodo = (id: number) => ({
-    type: REMOVE_TODO,
-    payload: id
-});
-
-type TodoAction =
-    | ReturnType<typeof addTodo>
-    | ReturnType<typeof toggleTodo>
-    | ReturnType<typeof removeTodo>
+const actions = {
+    addTodo,
+    removeTodo,
+    toggleTodo
+}
+type TodoAction = ActionType<typeof actions>;
 
 export type Todo = {
     id: number;
@@ -37,24 +36,80 @@ type TodosState = Todo[];
 
 const initialState: TodosState = [];
 
-function todos(state: TodosState = initialState
-               , action: TodoAction): TodosState {
-    switch (action.type) {
-        case ADD_TODO:
-            return state.concat({
-                id: action.payload.id,
-                text: action.payload.text,
-                done: false
-            });
-        case TOGGLE_TODO:
-            return state.map(todo =>
-                todo.id === action.payload ? {...todo, done: !todo.done} : todo
-            );
-        case REMOVE_TODO:
-            return state.filter(todo => todo.id !== action.payload);
-        default:
-            return state;
-    }
-}
+const todos = createReducer<TodosState, TodoAction>(initialState, {
+    [ADD_TODO]: (state, action) => state.concat({
+        ...action.payload,
+        done: false
+    }),
+    [TOGGLE_TODO]: (state, action) => state.map(
+        todo => todo.id === action.payload
+            ? {...todo, done: !todo.done}
+            : todo
+    ),
+    [REMOVE_TODO]: (state, action) => state.filter(
+        todo => todo.id !== action.payload
+    )
+})
 
 export default todos;
+
+// const ADD_TODO = 'todos/ADD_TODO' ;
+// const TOGGLE_TODO = 'todos/TOGGLE_TODO' ;
+// const REMOVE_TODO = 'todos/REMOVE_TODO' ;
+//
+// let nextId = 1;
+//
+// export const addTodo = (text: string) => ({
+//     type: ADD_TODO,
+//     payload: {
+//         id: nextId++,
+//         text
+//     }
+// })
+//
+// export const toggleTodo = (id: number) => ({
+//     type: TOGGLE_TODO,
+//     payload: id
+// });
+//
+// export const removeTodo = (id: number) => ({
+//     type: REMOVE_TODO,
+//     payload: id
+// });
+//
+// type TodoAction =
+//     | ReturnType<typeof addTodo>
+//     | ReturnType<typeof toggleTodo>
+//     | ReturnType<typeof removeTodo>
+//
+// export type Todo = {
+//     id: number;
+//     text: string;
+//     done: boolean;
+// }
+//
+// type TodosState = Todo[];
+//
+// const initialState: TodosState = [];
+//
+// function todos(state: TodosState = initialState
+//                , action: TodoAction): TodosState {
+//     switch (action.type) {
+//         case ADD_TODO:
+//             return state.concat({
+//                 id: action.payload.id,
+//                 text: action.payload.text,
+//                 done: false
+//             });
+//         case TOGGLE_TODO:
+//             return state.map(todo =>
+//                 todo.id === action.payload ? {...todo, done: !todo.done} : todo
+//             );
+//         case REMOVE_TODO:
+//             return state.filter(todo => todo.id !== action.payload);
+//         default:
+//             return state;
+//     }
+// }
+//
+// export default todos;
